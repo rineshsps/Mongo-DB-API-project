2ba91fd938828075605b7a935c6a64097a0d60b4
RINESH KM <rineshsps@live.com>
2021-06-18 22:14:51 +0530

    Update Startup.cs
    enable swagger for prod

--- Mongo.API/Mongo.API/Startup.cs
+++ Mongo.API/Mongo.API/Startup.cs
@@ -37,7 +37,7 @@ namespace Mongo.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+           // if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
